25dd0ea4172be3dc9aa5fcd0dfe0e011b75585b6
Alexandre Mendes da Costa <alexandremendes1005@gmail.com>
2025-05-18 11:43:33 -0300

    Ajustes

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS Access Key temporária (Academy)"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key temporária (Academy)"
-  type        = string
-}
-
-variable "aws_session_token" {
-  description = "AWS Session Token temporário (Academy)"
-  type        = string
-}
-
 variable "tabelas" {
   description = "Mapa de tabelas DynamoDB"
   type = map(object({

15d78e9b1fb59b50a636d71ba71f73827bc1f622
Alexandre Mendes da Costa <alexandremendes1005@gmail.com>
2025-05-18 17:01:46 -0300

    Atualizando nome do bucket

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
-backend "s3" {
-    bucket         = "soat-terraform-state-2025"
-    key            = "dynamo/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+  backend "s3" {
+    bucket  = "soat-terraform-state-5dc9264c8c230a834fe2a59b744523fd"
+    key     = "dynamo/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_dynamodb_table" "multi" {

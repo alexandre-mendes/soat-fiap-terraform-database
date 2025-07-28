d42b5387a412561a65cd648f370a52f9f4d14f4a
Alexandre Mendes <59848642+alexandre-mendes@users.noreply.github.com>
2025-07-28 10:00:43 -0300

    Update aurora.tf

--- aurora.tf
+++ aurora.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "aurora_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = ["sg-05684ba79bcf50773"]
+    security_groups = [aws_security_group.eks_sg.id]
     description     = "Permite trafego do EKS"
   }
 
